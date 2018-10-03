acd33d3f6b0eabce025f5f277cc7f89b877b81c5
Fabiano Brito <fabiano.brito@leadin.fi>
2018-10-03 21:23:25 +0300

    Create example of test with private method.

--- src/Test/TypingTest.test.js
+++ src/Test/TypingTest.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import TypingTest from '../Components/TypingTest';
 
-it('renders without crashing', () => {
-  shallow(<TypingTest />);
+it('finishes the typing test', () => {
+  const wrapper = shallow(<TypingTest />);
+  wrapper.instance().finishedFunction();
+  expect(wrapper.instance().wordsTest.started).toBe(false);
+  expect(wrapper.find('div').text()).toBe('You have finished');
 });
