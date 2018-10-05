42dcb48cd7bfbe9e27375d0d1b09d5d03adf91f2
scoombe <skatesam95@gmail.com>
2018-10-05 10:27:25 +0100

    Passing test

--- src/Test/Test.test.js
+++ src/Test/Test.test.js
@@ -6,5 +6,5 @@ it('finishes the typing test', () => {
   const wrapper = shallow(<Test />);
   wrapper.instance().finishedFunction();
   expect(wrapper.instance().wordsTest.started).toBe(false);
-  expect(wrapper.find('hq3').text()).toBe('Finished');
+  expect(wrapper.find('h3').text()).toBe('Finished');
 });
