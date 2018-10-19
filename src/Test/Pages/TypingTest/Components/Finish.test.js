0a54750eee40f303044e788d7b532fd527db7d0e
scoombe <skatesam95@gmail.com>
2018-10-19 16:15:35 +0100

    Finishing the finish component tests

--- src/Test/Pages/TypingTest/Components/Finish.test.js
+++ src/Test/Pages/TypingTest/Components/Finish.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Finish from '../../../../Pages/TypingTest/Components/Finish';
+import { wrap } from 'module';
 
 it('renders without crashing', () => {
   const props = {
@@ -13,4 +14,21 @@ it('renders without crashing', () => {
   };
 
   shallow(<Finish {...props} />);
+});
+
+it('has the right finish text', () => {
+  const props = {
+    restart: () => null,
+    wordCount: 90,
+    minutes: 1,
+    wpm: 90,
+    lastTenAvWPM: 90,
+    averageWPM: 85,
+  };
+  const wrapper = shallow(<Finish {...props} />);
+  expect(wrapper.find('h3').first().text()).toBe('You have Finished!');
+  expect(wrapper.find('li').first().text()).toBe('Words typed: 90 in 1 minutes');
+  expect(wrapper.find('li').at(1).text()).toBe('Your WPM: 90');
+  expect(wrapper.find('li').at(2).text()).toBe('Average words per minute(last 10 seconds): 90.00');
+  expect(wrapper.find('li').at(3).text()).toBe('Average words per minute(total): 85.00');
 });
--- src/Test/Pages/TypingTest/Components/Test.test.js
+++ src/Test/Pages/TypingTest/Components/Test.test.js
@@ -4,28 +4,23 @@ import Test from '../../../../Pages/TypingTest/Components/Test';
 
 const defaultProps = {
   getDisplayText: () => 'test',
+  checkKey: () => 'test',
+  startStopWatch: () => 'test',
 };
 
 it('renders without crashing', () => {
   shallow(<Test {...defaultProps} />);
 });
-
 it('calls componentDidMount once', () => {
   const spy = jest.spyOn(Test.prototype, 'componentDidMount');
-
   shallow(<Test {...defaultProps} />);
-
   expect(spy).toHaveBeenCalled();
   spy.mockRestore();
 });
-
 it('adds keydown event listener in componentDidMount', () => {
   const spy = jest.spyOn(document, 'addEventListener');
-
   Test.prototype.onInputChange = jest.fn();
-
   const wrapper = shallow(<Test {...defaultProps} />);
-
   expect(spy).toHaveBeenCalledWith('keydown', wrapper.instance().onInputChange, false);
   spy.mockRestore();
 });
