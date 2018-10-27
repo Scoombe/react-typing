015de7bf38b0959cf52f49ba15ddb8babdd17dba
Jake Nelson <jnelson180@gmail.com>
2018-10-26 23:04:23 -0500

    Added some more tests in App component

--- src/Test/Components/App/App.test.js
+++ src/Test/Components/App/App.test.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../../Components/App';
+import TypingHeader from '../../../Components/Header';
+import TypingTest from '../../../Pages/TypingTest';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+const wrapper = shallow(<App />);
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('renders a TypingHeader', () => {
+  expect(wrapper.find(TypingHeader).length).toBe(1);
+}); 
+
+it('renders a Router', () => {
+  expect(wrapper.find(Router).length).toBe(1);
+}); 
+
+it('renders a Switch', () => {
+  expect(wrapper.find(Switch).length).toBe(1);
+}); 
+
+it('renders a Route to "/"', () => {
+  expect(wrapper.contains(<Route exact path="/" component={TypingTest} />)).toBeTruthy();
 });
