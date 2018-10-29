f8453cc94e21a5e3cad4a2421c2bf8b857b6d7d3
scoombe <skatesam95@gmail.com>
2018-10-29 10:12:24 +0000

    Adding the firebase config and pages

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -6,18 +6,45 @@ import {
 } from 'react-router-dom';
 import TypingTest from '../../Pages/TypingTest';
 import TypingHeader from '../Header';
+import LoginPage from '../../Pages/LoginPage';
+import fire from '../../config/fire';
 
-function App() {
-  return (
-    <div className="App">
-      <TypingHeader />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={TypingTest} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+      else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TypingHeader />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={TypingTest} />
+            <Route exact path="/login" component={LoginPage} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
